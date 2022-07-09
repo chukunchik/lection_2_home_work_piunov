ec1ab1c0546c6753e6b93e826fca30195f5ff1d4
Piunov Volodymyr <108764098+chukunchik@users.noreply.github.com>
2022-07-09 21:48:28 +0300

    Add files via upload
    more questions

---
+++ lection_2_home_work.py
@@ -0,0 +1,56 @@
+age = None
+print(type(age))  # узнаем класс операнда
+
+x = [1, 2, 3]  # assignment operator(оператор присваивания)
+print(type(x))  # type list(тип список)
+
+h,j,k = 'moon',80,9.07
+print(h,j,k)
+
+y = 2 * 5  # matematic operator(математический оператор)
+print(y)
+
+y1 = 70 + 30
+print(y1)
+
+a = 5 > 8  # comparison operator(оператор сравнения)
+print(a)
+
+s = 8 > 20
+print(s)
+
+b = 5 > 2 and 10 < 20  # logic operator(логический оператор)
+print(b)
+
+f = 7 < 10 or 25 > 11
+print(f)
+
+e = 'cat'
+e = not 'dog'  # спросить в чем прикол этого оператора и как его применить
+print(e)
+
+c = 'boat' is 'boat'  # identity operator(оператор тождественности)
+print(c)
+
+j = 7 is 7    #ругается предлагает оператор равенства "=="
+print(j)
+
+cars = list['audi','ford','kia','vw']    # membership operator(оператор принадлежности)
+'bmw' in cars
+'vw' in cars
+
+'''
+
+СПРОСИТЬ ЧТО С 
+ОПЕРАТОРОМ in НУЖНО ДЕЛАТЬ
+чтобы определить true или false))) как вывести результат
+
+'''
+
+
+
+
+
+
+
+
